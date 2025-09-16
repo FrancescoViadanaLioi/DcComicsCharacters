92bbbc8908c29d937d65a79b273f8faa4194a45f
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-16 15:50:40 -0300

    Feat: Power entity.

---
+++ SourceCode/Entities/Attributes/Power.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DcCharacters.Entities.Attributes
+{
+    internal class Power
+    {
+        public string PowerName { get; set; }
+
+        public Power(string powerName)
+        {
+            PowerName = powerName;
+        }
+        public override string ToString() => PowerName;
+    }
+}
