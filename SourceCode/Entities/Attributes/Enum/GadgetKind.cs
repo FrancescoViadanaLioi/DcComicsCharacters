49f96d4133d79edf8ca08c1fa9c1291da2426b03
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-16 15:50:21 -0300

    Feat: Gadget entity and it's kinds.

---
+++ SourceCode/Entities/Attributes/Enum/GadgetKind.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DcCharacters.Entities.Attributes.Enum
+{
+    internal enum GadgetKind
+    {
+        Offensive,
+        Defensive,
+        Vehicle,
+        Locomotion,
+        HumanTech,
+        AlienTech,
+    }
+}
---
+++ SourceCode/Entities/Attributes/Gadget.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DcCharacters.Entities.Attributes.Enum;
+
+namespace DcCharacters.Entities.Attributes
+{
+    internal class Gadget
+    {
+        public string GadgetName { get; private set; }
+        public GadgetKind Kind {get; private set; }
+
+        public Gadget(string gadgetName, GadgetKind kind)
+        {
+            GadgetName = gadgetName;
+            Kind = kind;
+        }
+    }
+}
