90ecc8293f7a9a26262545f4a7c2129d9392d178
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-16 15:51:21 -0300

    Feat: Skill entity, such as powers, gadgets and more; they are goint to be stored in a list in the characters mother class.

---
+++ SourceCode/Entities/Attributes/Skill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DcCharacters.Entities.Attributes
+{
+    internal class Skill
+    {
+        public string Description { get; private set; }
+
+        public Skill(string description)
+        {
+            Description = description;
+        }
+    }
+}
