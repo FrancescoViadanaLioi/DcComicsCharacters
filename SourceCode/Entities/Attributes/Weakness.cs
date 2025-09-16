bf4c8703f6336bce51ca2ed7f9867ae4523d550b
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-16 15:56:39 -0300

    Feat: Weaknesses entity, which contains the description of the weaknesses.

---
+++ SourceCode/Entities/Attributes/Weakness.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DcCharacters.Entities.Attributes
+{
+    internal class Weakness
+    {
+        public string Description { get; private set; }
+
+        public Weakness(string description)
+        {
+            Description = description;
+        }
+    }
+}
